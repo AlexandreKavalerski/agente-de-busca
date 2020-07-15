c036a90d04b146cacc631cb31daf36c278b94a07
Alexandre <kavalerskialexandre@gmail.com>
2020-07-15 20:01:46 -0300

    Atualizada classe Position (atributos e funções)

--- BuscaEmLargura/Position.py
+++ BuscaEmLargura/Position.py
@@ -1,4 +1,11 @@
 class Position():
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, line, col):
+        self.line = line
+        self.col = col
+        
+    def __eq__(self, other):
+        return self.line == other.line and self.col == other.col
+    
+    def __str__(self):
+        return '({}, {})'.format(self.line, self.col) 
+        
